3b351ec7d04701db3b158855fa8b1e7f4b96073f
Nikola Nikolov <cmpax40@gmail.com>
2023-04-09 22:56:14 +0300

    Added basic endpoint and /update-sensor post

---
+++ expressjs_server/esp-api.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 8080;
+
+
+app.use(cors());
+
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/update-sensor', (req, res) => {
+    const data = req.body;
+    res.send("obicham made")
+//    console.log(data);
+
+    var parsedData = JSON.parse(JSON.stringify(data));
+    console.log(parsedData);
+    
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World, from express');
+});
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
